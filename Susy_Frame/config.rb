de9cfcdfbabb5feac176651909926a160fc30b61
bing <tenexbing@gmail.com>
2015-01-08 00:17:19 +0800

    index

--- Susy_Frame/config.rb
+++ Susy_Frame/config.rb
@@ -7,7 +7,6 @@ css_dir = "css"
 sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "javascripts"
-
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
 
